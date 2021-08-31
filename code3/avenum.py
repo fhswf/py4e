ec20fbcee98e373435929eaa5aaa1425f5b09477
Heiner Giefers <giefers.heiner@fh-swf.de>
2021-08-31 13:42:20 +0200

    Rev 08-lists

--- code3/avenum.py
+++ code3/avenum.py
@@ -1,11 +1,11 @@
 total = 0
 count = 0
 while (True):
-    inp = input('Enter a number: ')
+    inp = input('Gib eine Zahl ein: ')
     if inp == 'done': break
     value = float(inp)
     total = total + value
     count = count + 1
 
 average = total / count
-print('Average:', average)
+print('Mittelwert:', average)
